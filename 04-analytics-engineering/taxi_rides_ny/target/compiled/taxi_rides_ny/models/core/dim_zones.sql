d789b9745847daa47fe0ce9a274e332a3e5f7b46
Mariel Ambratis <valofils@gmail.com>
2026-04-20 14:21:38 +0000

    chore: gitignore dbt target, packages, logs

--- 04-analytics-engineering/taxi_rides_ny/target/compiled/taxi_rides_ny/models/core/dim_zones.sql
+++
@@ -1,9 +0,0 @@
-
-
-select
-    cast(locationid as integer)  as locationid,
-    borough,
-    zone,
-    replace(service_zone, 'Boro', 'Green') as service_zone
-
-from `food-security-pipeline`.`ny_taxi`.`taxi_zone_lookup`
